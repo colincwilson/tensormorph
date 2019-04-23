96e43fc90be11b476ef6d59f056f1b310c95787c
Colin Wilson <colin.wilson.jhu@gmail.com>
2019-04-23 09:42:31 -0400

    added Bahdanau et al. (2014) model

---
+++ bahdanau_model.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Encoder-Decoder (after Bahdanau et al. 2014)
+import torch
+import torch.nn as nn
+import onmt
+from onmt.utils.misc import sequence_mask
+
+class BahdanauDecoder(nn.Module):
+    def __init__(self, embedding, nhidden):
+        super(BahdanauDecoder, self).__init__()
+        nsym = embedding.nsym
+        self.embedding = embedding
+        self.Ws = nn.Linear(nhidden, nhidden)           # state initialization
+        self.Wz = nn.Linear(nsym+nhidden*2, nhidden)    # state update
+        self.Wr = nn.Linear(nsym+nhidden*2, nhidden)    # state reset
+        self.Wp = nn.Linear(nsym+nhidden*2, nhidden)    # state proposal
+        self.Wa = nn.Linear(2*nhidden, nhidden)         # attention
+        self.va = nn.Linear(nhidden, 1, bias=False)     #
+        self.Wo = nn.Linear(nsym+nhidden*2, nhidden)    # output
+        self.nsym = nsym
+        self.nhidden = nhidden
+
+    def forward(self, src_enc, tgt, src_lengths):
+        dec_outputs = []
+        dec_states = []
+        dec_attns = []
+        tgt_len = tgt.shape[0]
+        src_mask = sequence_mask(src_lengths, max_len=src_enc.size(0)).transpose(0,1)
+
+        # precompute all target-side embeddings
+        embed = self.embedding(tgt.squeeze(-1))
+        # initialize decoder state
+        s = torch.tanh(self.Ws(src_enc[0,:,:]))
+        # recurrence
+        for i in range(1,tgt_len):
+            # condition attention on current decoder state
+            s_ = s.unsqueeze(0).expand(src_enc.shape[0],
+                                        s.shape[0],
+                                        s.shape[1]) # xxx
+            inpt = torch.cat([s_, src_enc], -1)
+            ei = self.va(torch.tanh(self.Wa(inpt))).squeeze(-1)
+            ei = ei.masked_fill(1 - src_mask, -float('inf'))
+            ai = torch.exp(torch.log_softmax(ei, 0))
+            #print (ai.shape, src_enc.shape); sys.exit(0)
+            ci = torch.sum(ai.unsqueeze(-1) * src_enc, 0)
+
+            # update decoder state
+            inpt = torch.cat([embed[i-1,:,:], s, ci], -1)
+            zi = torch.sigmoid(self.Wz(inpt))   # update
+            ri = torch.sigmoid(self.Wr(inpt))   # reset
+            inpt = torch.cat([embed[i-1,:,:], ri * s, ci], -1)
+            si = torch.tanh(self.Wp(inpt))      # proposal
+            s = (1.0 - zi) * s  +  zi * si      # new state
+
+            # compute decoder output
+            inpt = torch.cat([embed[i-1,:,:], s, ci], 1)
+            ti = self.Wo(inpt)
+
+            # store decoder state and output, attention distributions
+            dec_states += [s.clone().unsqueeze(0),]
+            dec_outputs += [ti.clone().unsqueeze(0),]
+            dec_attns += [ai.clone().transpose(0,1).unsqueeze(0),]
+
+        dec_outputs = torch.cat(dec_outputs)    # (tgt_len, batch_len, nhidden)
+        dec_states = torch.cat(dec_states)      # (tgt_len, batch_len, nhidden)
+        dec_attns = torch.cat(dec_attns)        # (tgt_len, batch_len, src_lengths)
+        return dec_outputs, dec_states, dec_attns
+
+
+class BahdanauModel(nn.Module):
+    def __init__(self, embedding, nhidden):
+        super(BahdanauModel, self).__init__()
+        self.nsym = embedding.nsym
+        self.nhidden = nhidden
+        self.embedding = embedding
+        self.encoder = onmt.encoders.RNNEncoder(
+            rnn_type = 'GRU',
+            bidirectional = True,
+            num_layers = 1,
+            hidden_size = self.nhidden,
+            dropout = 0.0,
+            embeddings = embedding)
+        self.decoder = BahdanauDecoder(
+            embedding,
+            nhidden)
+        self.generator = nn.Sequential(
+            nn.Linear(nhidden, embedding.nsym),
+            nn.LogSoftmax(dim=-1))
+
+    def forward(self, src, tgt, src_lengths, bptt=False):
+        # encode source
+        _, src_enc, _ = self.encoder(src)
+        # decode
+        dec_outputs, dec_states, dec_attns =\
+            self.decoder(src_enc, tgt, src_lengths)
+        # generate from decoder outputs
+        gen_outputs = self.generator(dec_outputs)
+        
+        return gen_outputs
--- encoder_decoder.py
+++ encoder_decoder.py
@@ -6,14 +6,17 @@ import torch
 import torch.nn as nn
 import onmt
 from onmt.modules.embeddings import Embeddings
+import bahdanau_model
+import pandas as pd
+import numpy as np
 from collections import namedtuple
-import string
+import random, re, string, sys
 
 # # # # # # # # # #
 # Symbols, mapping from symbols to ids, and one-hot embeddings
 stem_begin, stem_end = '⋊', '⋉'
-syms = [stem_begin,] + [x for x in string.ascii_lowercase] + [stem_end,]
-sym2id = {syms[i]:(i+1) for i in range(len(syms))}
+syms = ['ε', stem_begin,] + [x for x in string.ascii_lowercase] + [stem_end,]
+sym2id = {syms[i]:i for i in range(len(syms))}
 
 def string2delim(x):
     y = [stem_begin,] + x.split(' ') + [stem_end,]
@@ -26,7 +29,6 @@ def string2idvec(x, delim=True):
 
 def strings2idmat(xs):
     ys = [string2delim(x) for x in xs]
-    ys.sort(key = lambda y : len(y), reverse=True)
     lens = torch.LongTensor([len(y) for y in ys])
     ids = [string2idvec(y) for y in ys]
     ids = nn.utils.rnn.pad_sequence(ids, batch_first=False, padding_value=0)
@@ -38,103 +40,190 @@ def strings2idmat(xs):
 class OneHotEmbeddings(Embeddings):
     def __init__(self, syms):
         super(Embeddings, self).__init__()
-        F = torch.eye(len(syms)+1, requires_grad=False)
-        F[0,0] = 0.0
-        self.F = nn.Embedding.from_pretrained(F, freeze=True)
-        self.syms = ['ε',] + syms
-        self.nsym = len(self.syms)
-        self.embedding_size = F.shape[0] # xxx check
+        self.F = torch.eye(len(syms))
+        self.F[0,0] = 0
+        self.embedding = nn.Embedding(len(syms), len(syms), padding_idx=0)
+        self.embedding.weight = torch.nn.Parameter(self.F, requires_grad=False)
+        self.syms = syms
+        self.nsym = self.embedding.num_embeddings
+        self.embedding_size = self.embedding.embedding_dim
+        self.padding_idx = self.embedding.padding_idx
     
     def forward(self, source, step=None):
-        X = self.F(source.squeeze(-1))
-        return X
+        source_embed = self.embedding(source.squeeze(-1))
+        return source_embed
 
-embedding = OneHotEmbeddings(syms)
-stems = ['l i g h t', 'd a r k']
-stem_ids, stem_lens = strings2idmat(stems)
-#Stems = embedding(stem_ids)
-#print (Stems.shape) # (max_len, batch, embedding_size)
-outputs = ['l i g h t n e s s', 'd a r k n e s s']
-output_ids, output_lens = strings2idmat(outputs)
-Outputs = embedding(output_ids)
+# # # # # # # # # #
+# Data set, batches, batch generator
+dat = pd.read_csv('/Users/colin/Dropbox/TensorProductStringToStringMapping/english/english_ness.csv')
+dat['stem_len'] = [len(x) for x in dat['stem']]
+dat['output_'] = [x+'ness' for x in dat['stem']]
+dat = dat[(dat['output'] == dat['output_'])]
+dat = dat[(dat['stem_len'] < 8)]
+stems = [' '.join(x) for x in dat['stem']]
+outputs = [' '.join(x) for x in dat['output']]
 
+Batch = namedtuple('Batch', ['src', 'tgt', 'batch_size'])
+batch_size = 32
+nbatch = int(np.ceil(len(stems) / batch_size))
+batch_indx = np.repeat([k for k in range(nbatch)], batch_size)
+batch_indx = random.sample([k for k in batch_indx], len(stems))
+batches = []
+for k in range(nbatch):
+    examples = [(stems[i], outputs[i]) for i in range(len(stems))\
+        if batch_indx[i] == k]
+    examples.sort(key = lambda x : len(x[0]), reverse=True)
+    stems_, outputs_ = zip(*examples)
+    stem_ids, stem_lens = strings2idmat(stems_)
+    output_ids, output_lens = strings2idmat(outputs_)
+    batches.append(
+        Batch((stem_ids, stem_lens), output_ids, stem_lens.shape[0])
+    )
+train_iter = batches
 
 # # # # # # # # # #
 # Encoder-Decoder model
 device = "cuda" if torch.cuda.is_available() else "cpu"
+main_dir = '/Users/colin/Desktop/'
 hidden_size = 50
-learning_rate = 1
+train_model = True
+learning_rate = 0.1
+max_grad_norm = 2
 
-encoder = onmt.encoders.RNNEncoder(
-    rnn_type='GRU',
-    bidirectional=True,
-    num_layers=1,
-    hidden_size=hidden_size,
-    dropout=0.0,
-    embeddings=embedding)
+embedding = OneHotEmbeddings(syms)
 
-#h, H, _ = encoder.forward(stem_ids, stem_lens)
-#print (h)
-
-decoder = onmt.decoders.StdRNNDecoder(
-    rnn_type='GRU',
-    bidirectional_encoder=True,
-    num_layers=1,
-    hidden_size=hidden_size,
-    attn_type='mlp',
-    attn_func='softmax',
-    dropout=0.0,
-    embeddings=embedding
+model = bahdanau_model.BahdanauModel(
+    embedding,
+    hidden_size
 )
 
-model = onmt.models.NMTModel(
-    encoder,
-    decoder)
-model.to(device)
-#print (model)
+loglik_loss = nn.NLLLoss(ignore_index=0, reduction="sum")
 
-#Pred,_ = model.forward(stem_ids, output_ids, stem_lens)
-#print (Pred.shape)
+def batch_loss(gen_outputs, output_ids):
+    output_len, batch_len, _ = gen_outputs.shape
+    loss = loglik_loss(
+        gen_outputs.view(output_len*batch_len, -1),
+        output_ids[1:,:,:].view(-1)
+    ) # note: combine output posn and batch dimensions
+    return loss
 
-model.generator = nn.Sequential(
-    nn.Linear(hidden_size, embedding.nsym),
-    nn.LogSoftmax(dim=-1)).to(device)
+def batch2syms(batch_ids, add_stem_begin=False):
+    nout, nbatch = batch_ids.shape
+    xs = []
+    for i in range(nbatch):
+        xi = [syms[batch_ids[j,i]] for j in range(nout)]
+        if add_stem_begin:
+            xi = [stem_begin,] + xi
+        xi = ' '.join(xi)
+        xi = re.sub(stem_end+'.*', stem_end, xi)
+        xs.append(xi)
+    return xs
 
-loss = onmt.utils.loss.NMTLossCompute(
-    criterion=nn.NLLLoss(ignore_index=0, reduction="sum"),
-    generator=model.generator)
+optim = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-3)
 
-optim = onmt.utils.optimizers.Optimizer(
-    torch.optim.SGD(model.parameters(), lr=learning_rate),
-    learning_rate=learning_rate,
-    max_grad_norm=2)
+def train(model, batch_loss, optim, batches):
+    total_loss = 0.0
+    for batch in random.sample(batches, len(batches)):
+        stem_ids, stem_lengths = batch.src
+        output_ids = batch.tgt
+        gen_outputs = model(stem_ids, output_ids, stem_lengths)
+        loss = batch_loss(gen_outputs, output_ids)
+        total_loss += loss.item()
 
-report_manager = onmt.utils.ReportMgr(
-    report_every=1,
-    start_time=None,
-    tensorboard_writer=None)
+        model.zero_grad()
+        loss.backward()
+        nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
+        optim.step()
+    print (total_loss)
+
+nepoch = 100
+for epoch in range(nepoch):
+    train(model, batch_loss, optim, train_iter)
+
+# testing
+stem_ids, stem_lengths = train_iter[0].src
+output_ids = train_iter[0].tgt
+_, stem_enc, _ = model.encoder(stem_ids)
+dec_outputs, dec_states, dec_attns =\
+    model.decoder(stem_enc, output_ids, stem_lengths)
+gen_outputs =\
+    model.generator(dec_outputs)
+_, gen_ids = torch.max(gen_outputs, 2)
+#print (np.round(gen_outputs[:,-1,:].data.numpy(), 3))
+#print (torch.max(dec_attns[:,-1,:], 1))
+
+targs = batch2syms(output_ids.squeeze(-1))
+preds = batch2syms(gen_ids, add_stem_begin=True)
+targ_preds = zip(targs, preds)
+for x in targ_preds:
+    print (x)
+
+
+sys.exit(0)
+
+
+# # # # # OLD CODE # # # # #
+
+model_saver = onmt.models.ModelSaver(
+    base_path = '/Users/colin/Desktop/',
+    model = model,
+    model_opt = 'NA',
+    fields = 'NA',
+    optim = optim)
 
 trainer = onmt.Trainer(
-    model=model,
-    train_loss=loss,
-    valid_loss=loss,
-    optim=optim,
-    report_manager=report_manager)
+    model = model,
+    train_loss = loss,
+    valid_loss = loss,
+    optim = optim,
+    report_manager = report_manager,
+    model_saver = None)
 
-Batch = namedtuple('Batch', ['src', 'tgt', 'batch_size'])
-batch = Batch((stem_ids, stem_lens), output_ids, stem_lens.shape[0])
-train_iter = [batch,]*100
+if train_model:   # train model
+    print ('training ...')
+    for epoch in range(1000):
+        stats = trainer.train(
+            train_iter = train_iter,
+            train_steps = 10,
+            valid_iter = train_iter,
+            valid_steps = 10,
+            save_checkpoint_steps = 1.0e8)
+        if epoch % 100 == 0:
+            print ('loss =', stats.loss, 'n_words =', stats.n_words, 'n_correct =', stats.n_correct)
 
-print ('training ...')
-trainer.train(
-    train_iter=train_iter,
-    train_steps=100,
-    valid_iter=None,
-    valid_steps=0)
+    for param_tensor in model.state_dict():
+        print(param_tensor, "\t", model.state_dict()[param_tensor].size())
+    torch.save(model.state_dict(), main_dir +'encoder_decoder_params.pt')
+else:   # restore model
+    params = torch.load(main_dir +'encoder_decoder_params.pt')
+    model.load_state_dict(params)
 
-Pred, _ = model.forward(stem_ids, output_ids, stem_lens)
+    _, encodings, _ = model.encoder(batches[0].src[0], batches[0].src[1])
+    torch.save(encodings, main_dir +'input_encodings.pt')
+    torch.save(batches[0].src[0], main_dir +'input_symbols.pt')
+sys.exit(0)
+
+
+print ('testing ...')
+for batch in train_iter:
+    print (batch.src[0].shape)
+    print (batch.src[1])
+    print (batch.tgt.shape)
+    Pred, attns = model(batch.src[0], batch.tgt, batch.src[1])
+    pred_max = torch.argmax(Pred, 2)
+    print (pred_max)
+    sys.exit(0)
+
+
+Pred, attns = model.forward(stem_ids, output_ids, stem_lens)
 Pred = model.generator(Pred) 
 print (Pred.shape)
-pred_max = torch.argmax(Pred, 2)
+pred_max = torch.argmax(Pred, -1)
 print (pred_max)
 print (output_ids.squeeze(-1))
+
+h, H, _ = encoder.forward(stem_ids, stem_lens)
+H = np.round(H.data, 3)
+print (H[:,0,:])
+
+torch.save(attns, maindir +'encoder_decoder_attns.pt')
