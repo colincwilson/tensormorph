e1a93f491263dda6627dc565ba696f928ece6692
Colin Wilson <colin@cogsci.jhu.edu>
2019-05-10 16:30:37 -0400

    handle sequences of different lengths in GRU

--- recurrent_layers.py
+++ recurrent_layers.py
@@ -22,7 +22,7 @@ class GRU1(nn.Module):
     
     def forward(self, input, input_len=None):
         max_len, batch_size, _ = input.shape
-        h_init = torch.zeros(batch_size, self.hidden_size)
+        h_init = torch.zeros(batch_size, self.hidden_size, requires_grad=False)
         R, Z, N, H = [], [], [], []
         for t in range(max_len):
             xt = input[t,:,:]
@@ -30,20 +30,23 @@ class GRU1(nn.Module):
             rt = torch.sigmoid(self.W_ir(xt) + self.W_hr(h_prev))
             zt = torch.sigmoid(self.W_iz(xt) + self.W_hz(h_prev))
             nt = torch.tanh(self.W_in(xt) + rt * self.W_hn(h_prev))
-            ht = (1. - zt) * nt + zt * h_prev
+            ht = (1.0 - zt) * nt + zt * h_prev
             R.append(rt); Z.append(zt); N.append(nt); H.append(ht)
 
         for name,result in zip(['R','Z','N','H'], [R,Z,N,H]):
             setattr(self, name, torch.stack(result,0))
         if input_len is not None:
             self.mask_results(input_len)
+            print (self.H.shape, input_len.shape)
+            print (input_len)
+            return self.H[input_len-1, torch.arange(self.H.shape[0])]
         return self.H
 
     def mask_results(self, input_len):
         batch_size = input_len.shape[0]
         mask = torch.zeros_like(self.H).byte()
         for i in range(batch_size):
-            mask[:input_len[i,0],i,:] = 1
+            mask[:input_len[i],i,:] = 1
         for name in ['R', 'Z', 'N', 'H']:
             result = getattr(self, name)
             result.masked_fill_(1-mask, 0.0)
@@ -56,9 +59,10 @@ def main():
     gru1 = GRU1(input_size, hidden_size)
 
     inpt = torch.rand(max_len, batch_size, input_size)
-    inpt_len = torch.arange(1,batch_size+1).unsqueeze(-1)
+    inpt_len = torch.as_tensor([1,3,2])
     H = gru1(inpt, inpt_len)
     print (H.shape)
+    sys.exit(0)
     for i in range(batch_size):
         print (H[:,i,:])
         print (gru1.Z[:,i,:])
