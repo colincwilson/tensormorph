95623eb23ac58a3d79b16b642305e778f5dcd01e
Colin Wilson <colin@cogsci.jhu.edu>
2019-06-10 16:48:43 -0400

    Remove constant/redundant features

--- symbol_embedder.py
+++ symbol_embedder.py
@@ -30,7 +30,7 @@ class SymbolEmbedder():
             if isinstance(feature_matrix, str):
                 feature_matrix = pd.read_csv(feature_matrix)
             (syms, ftrs, nfill, dfill, F) =\
-                self.parse_feature_matrix(feature_matrix)
+                self.parse_feature_matrix(feature_matrix, segments)
         else:
             (syms, ftrs, nfill, dfill, F) =\
                 self.make_embedding(segments, vowels, randvec_params)
@@ -40,25 +40,53 @@ class SymbolEmbedder():
         config.dfill    = dfill
         config.F        = F
 
-
-    def parse_feature_matrix(self, ftr_matrix):
+    # xxx todo: split off
+    def parse_feature_matrix(self, ftr_matrix, segments=None):
         """
-        Read features from a pandas DataFrame with initial column named 'segment', 
+        Read features from a pandas DataFrame with initial column containing segments,  
         detecting the feature that distinguishes vowels from consonants.
         """
-        epsilon     = config.epsilon
-        stem_begin  = config.stem_begin
-        stem_end    = config.stem_end
-        segments    = [x for x in ftr_matrix.segment]
-        features    = [x for x in ftr_matrix.columns[1:]]
-        vowel_ftr   = [ftr for ftr in features if re.match('^(syll|syllabic)$', ftr)][0]
-        vowels      = [x for i,x in enumerate(segments) if ftr_matrix[vowel_ftr][i]=='+']
-        consonants  = [x for x in segments if x not in vowels] # true consonants and glides
+        epsilon         = config.epsilon
+        stem_begin      = config.stem_begin
+        stem_end        = config.stem_end
+        segments_all    = [x for x in ftr_matrix.iloc[:,0]]
+        features_all    = [x for x in ftr_matrix.columns[1:]]
+        vowel_ftr       = [ftr for ftr in features_all if re.match('^(syl|syllabic)$', ftr)][0]
 
+        # Reduce feature matrix to given segments (optional), pruning features 
+        # other than vowel_ftr that have constant or redundant values
+        if segments is not None:
+            segments = [x for x in segments_all if x in segments]
+            indx1 = [i for i,x in enumerate(segments_all) if x in segments]
+            ftr_matrix = ftr_matrix.iloc[indx1,:].reset_index()
+
+            features_drop = []
+            for j,ftr in enumerate(features_all):
+                if ftr==vowel_ftr:
+                    continue
+                if ftr_matrix[ftr].nunique()<2:
+                    features_drop.append(ftr)
+                    continue
+                for k in range(len(features_all)):
+                    if all(ftr_matrix.iloc[:,j]==ftr_matrix.iloc[:,k]):
+                        features_drop.append(ftr)
+                        break
+            
+            features = [ftr for ftr in features_all if ftr not in features_drop]
+            ftr_matrix = ftr_matrix.loc[:,features].reset_index()
+            print ('features:', features, '\n', ftr_matrix)
+        else:
+            segments = segments_all 
+            features = features_all
+
+        print (ftr_matrix[vowel_ftr])
         syms    = [epsilon, stem_begin] + segments + [stem_end,]
+        vowels  = [x for i,x in enumerate(segments) if ftr_matrix[vowel_ftr][i]=='+']
+        consonants = [x for x in segments if x not in vowels]
         ftrs    = ['sym', 'begin', 'end', 'C', 'V'] + features
         nfill   = len(syms)     # segments + special symbols
         dfill   = len(ftrs)     # ordinary + special features
+        print (syms, vowels, consonants, ftrs, nfill, dfill)
 
         F = torch.zeros((dfill, nfill))
         F.data[0,1:]    = 1.0   # non-epsilon feature
